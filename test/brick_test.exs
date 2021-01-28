49242b7f795211855e9589366d61869c51eed275
Guilherme <guilhermemolina00@gmail.com>
2021-01-28 16:20:59 -0300

    First commit

---
+++ test/brick_test.exs
@@ -0,0 +1,21 @@
+defmodule BrickTest do
+  use ExUnit.Case
+
+  import Tetris.Brick
+ 
+  def new_brick, do: new()
+
+  test "Creates a new brick" do
+    assert new_brick().name == :i
+  end
+
+  test "Creates a new random brick" do
+      actual = new_random()
+
+      assert actual.name in [:i, :l, :z, :t, :o]
+      assert actual.rotation in [0, 90, 180, 270]
+      assert actual.reflection in [true, false]
+  end
+
+  
+end
