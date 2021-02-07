3c61905d5e66351c67bdbded3fad056ff14217b5
Guilherme <guilhermemolina00@gmail.com>
2021-02-06 22:14:26 -0300

    Points' colors

--- lib/points.ex
+++ lib/points.ex
@@ -2,8 +2,7 @@ defmodule Tetris.Points do
   @type t :: [{integer, integer}]
 
   ## Fn to controll bricks' points translation by x and y
-  @spec translate(t, {integer, integer}) :: t
-  def translate(points, {x, y}) do
+  def move_to_location(points, {x, y}=_location) do
     Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end)
   end
 
@@ -48,6 +47,13 @@ defmodule Tetris.Points do
 
     ## Repeat rotate till degrees == 0
   end
+
+  def with_color(points, color) do
+    Enum.map(points, fn point -> add_color(point, color) end)
+  end
+
+  defp add_color({_x, _y, _c} = point, _color), do: point
+  defp add_color({x, y}, color), do: {x, y, color}
 
   def to_string(points) do
     map =
